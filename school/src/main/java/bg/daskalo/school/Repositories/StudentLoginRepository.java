e6f1cfc927298f53838c1a0a5da49b5bbeab69e5
bloommyy <bloomy@abv.bg>
2022-01-01 19:19:58 +0200

    Renamed and changed return type of  fetch by password method.

--- school/src/main/java/bg/daskalo/school/Repositories/StudentLoginRepository.java
+++ school/src/main/java/bg/daskalo/school/Repositories/StudentLoginRepository.java
@@ -3,6 +3,8 @@ package bg.daskalo.school.Repositories;
 import bg.daskalo.school.Entities.Login.StudentLogin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudentLoginRepository extends JpaRepository<StudentLogin, Long> {
-    StudentLogin findStudentLoginByPassword(String password);
+    List<StudentLogin> findStudentsLoginByPassword(String password);
 }
