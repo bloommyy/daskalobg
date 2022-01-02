2ab7e5b62e2816efc699d56ee3003b632a60c882
Alexander Dimitrov <72303198+AlexDim1@users.noreply.github.com>
2022-01-02 17:43:54 +0200

    Changed Teacher to CamelCase

--- school/src/main/java/bg/daskalo/school/Entities/Teacher.java
+++ school/src/main/java/bg/daskalo/school/Entities/Teacher.java
@@ -8,28 +8,28 @@ import javax.persistence.*;
 public class Teacher {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long t_id;
+    private Long id;
 
-    private String t_first_name;
-    private String t_middle_name;
-    private String t_last_name;
+    private String firstName;
+    private String middleName;
+    private String lastName;
 
     @OneToOne
-    private TeacherLogin t_login;
+    private TeacherLogin login;
 
-    private String t_email;
-    private Integer t_subject_id;
+    private String email;
+    private Integer subjectId;
 
     @OneToOne(mappedBy = "sj_teacher")
-    private Subject t_subject;
+    private Subject subject;
 
-    public Teacher(String t_first_name, String t_middle_name, String t_last_name,
-                   String t_email, Integer t_subject_id) {
-        this.t_first_name = t_first_name;
-        this.t_middle_name = t_middle_name;
-        this.t_last_name = t_last_name;
-        this.t_email = t_email;
-        this.t_subject_id = t_subject_id;
+    public Teacher(String firstName, String middleName, String lastName,
+                   String email, Integer subjectId) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.email = email;
+        this.subjectId = subjectId;
     }
 
     public Teacher() {
@@ -37,54 +37,54 @@ public class Teacher {
     }
 
     public Long getId() {
-        return t_id;
+        return id;
     }
 
     public String getFirstName() {
-        return t_first_name;
+        return firstName;
     }
 
-    public void setFirstName(String t_first_name) {
-        this.t_first_name = t_first_name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     public String getMiddleName() {
-        return t_middle_name;
+        return middleName;
     }
 
-    public void setMiddleName(String t_middle_name) {
-        this.t_middle_name = t_middle_name;
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
     }
 
     public String getLastLame() {
-        return t_last_name;
+        return lastName;
     }
 
-    public void setLastName(String t_last_name) {
-        this.t_last_name = t_last_name;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getEmail() {
-        return t_email;
+        return email;
     }
 
-    public void setEmail(String t_email) {
-        this.t_email = t_email;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public Integer getSubjectId() {
-        return t_subject_id;
+        return subjectId;
     }
 
-    public void setSubjectId(Integer t_subject_id) {
-        this.t_subject_id = t_subject_id;
+    public void setSubjectId(Integer subjectId) {
+        this.subjectId = subjectId;
     }
 
     public Subject getSubject() {
-        return t_subject;
+        return subject;
     }
 
-    public void setSubject(Subject t_subject) {
-        this.t_subject = t_subject;
+    public void setSubject(Subject subject) {
+        this.subject = subject;
     }
 }
