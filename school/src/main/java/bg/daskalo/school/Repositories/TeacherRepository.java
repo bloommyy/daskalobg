81aa8904c173ec6d655acd911efde14b0d0a619a
Alexander Dimitrov <72303198+AlexDim1@users.noreply.github.com>
2022-01-02 17:59:11 +0200

    Added TeacherRepository

---
+++ school/src/main/java/bg/daskalo/school/Repositories/TeacherRepository.java
@@ -0,0 +1,8 @@
+package bg.daskalo.school.Repositories;
+
+import bg.daskalo.school.Entities.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+    Teacher findTeacherByEmail(String email);
+}
