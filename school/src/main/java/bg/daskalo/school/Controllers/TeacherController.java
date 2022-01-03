c143e669c4be44db203bc52b01eef580b1661278
Alexander Dimitrov <72303198+AlexDim1@users.noreply.github.com>
2022-01-03 18:39:53 +0200

    Added TeacherController and registration endpoint

---
+++ school/src/main/java/bg/daskalo/school/Controllers/TeacherController.java
@@ -0,0 +1,110 @@
+package bg.daskalo.school.Controllers;
+
+import bg.daskalo.school.Entities.Login.TeacherLogin;
+import bg.daskalo.school.Entities.Student;
+import bg.daskalo.school.Entities.Subject;
+import bg.daskalo.school.Entities.Teacher;
+import bg.daskalo.school.Payload.Request.PersistTeacherRequest;
+import bg.daskalo.school.Repositories.TeacherLoginRepository;
+import bg.daskalo.school.Repositories.TeacherRepository;
+import bg.daskalo.school.Utils.Security;
+import org.apache.commons.validator.routines.EmailValidator;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/teachers")
+public class TeacherController {
+    private final TeacherRepository teacherRepo;
+    private final TeacherLoginRepository teacherLoginRepo;
+
+    public TeacherController(TeacherRepository teacherRepo, TeacherLoginRepository teacherLoginRepo) {
+        this.teacherRepo = teacherRepo;
+        this.teacherLoginRepo = teacherLoginRepo;
+    }
+
+    private static boolean validateRegistration(String fname,
+                                                String mname,
+                                                String lname,
+                                                String email,
+                                                String password) {
+
+        if (fname == null ||
+                mname == null ||
+                lname == null ||
+                email == null ||
+                password == null)
+            return false;
+
+        if (fname.isEmpty() ||
+                mname.isEmpty() ||
+                lname.isEmpty() ||
+                email.isEmpty() ||
+                password.isEmpty())
+            return false;
+
+        for (char c : fname.toCharArray()) {
+            if (!Character.isLetter(c))
+                return false;
+        }
+
+        for (char c : mname.toCharArray()) {
+            if (!Character.isLetter(c))
+                return false;
+        }
+
+        for (char c : lname.toCharArray()) {
+            if (!Character.isLetter(c))
+                return false;
+        }
+
+        if (!EmailValidator.getInstance(true).isValid(email))
+            return false;
+
+        return password.length() >= 8;
+    }
+
+    @Transactional
+    @PostMapping("/register")
+    public ResponseEntity<?> persistTeacher(@RequestBody PersistTeacherRequest request) throws NoSuchAlgorithmException {
+        Teacher t = teacherRepo.findTeacherByEmail(request.getEmail());
+
+        if (t != null) {
+            return ResponseEntity.ok("That teacher is already registered.");
+        }
+
+        if (!validateRegistration(request.getFirstName(),
+                request.getMiddleName(),
+                request.getLastName(),
+                request.getEmail(),
+                request.getPassword()))
+            return ResponseEntity.ok("Teacher wasn't registered: One or more invalid parameters.");
+
+        String hashedPass = Security.encrypt(request.getPassword());
+
+        t = new Teacher(request.getFirstName(),
+                request.getMiddleName(),
+                request.getLastName(),
+                request.getEmail());
+
+        TeacherLogin tLogin = new TeacherLogin(t, hashedPass);
+
+        try {
+            teacherRepo.save(t);
+            teacherLoginRepo.save(tLogin);
+        } catch (Exception e) {
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+            return ResponseEntity.ok("An error occurred and the teacher wasn't registered!");
+        }
+
+        return new ResponseEntity<>("Registered new teacher " + t.getFirstName() + " "
+                + t.getMiddleName().charAt(0) + ". " + t.getLastName() + "!", HttpStatus.CREATED);
+    }
+}
---
+++ school/src/main/java/bg/daskalo/school/Payload/Request/PersistTeacherRequest.java
@@ -0,0 +1,63 @@
+package bg.daskalo.school.Payload.Request;
+
+import bg.daskalo.school.Entities.Subject;
+
+public class PersistTeacherRequest {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String subject;
+    private String email;
+    private String password;
+
+    public PersistTeacherRequest() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
