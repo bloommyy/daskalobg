0fb4737d922f779c7695abc5eab98345c25e304b
Katerina1203 <61545399+Katerina1203@users.noreply.github.com>
2022-01-08 14:48:21 +0200

    adding java.util.Optional;

--- school/src/main/java/bg/daskalo/school/Repositories/FeedbackRepository.java
+++ school/src/main/java/bg/daskalo/school/Repositories/FeedbackRepository.java
@@ -6,7 +6,10 @@ import bg.daskalo.school.Entities.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FeedbackRepository extends JpaRepository<Absence, Long> {
     List<Absence> findAllByStudentAndSubject(Student student, Subject subject);
+    
+    Optional<Absence> findAbsenceByStudentAndAndSubject(Student student, Subject subject);
 }
