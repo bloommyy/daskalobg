2ee95d7f525517ca7540d1a0adf9f10b708be7ba
Katerina1203 <61545399+Katerina1203@users.noreply.github.com>
2022-01-08 14:49:59 +0200

    adding java.util.Optional;

--- school/src/main/java/bg/daskalo/school/Repositories/AbsenceRepository.java
+++ school/src/main/java/bg/daskalo/school/Repositories/AbsenceRepository.java
@@ -6,7 +6,9 @@ import bg.daskalo.school.Entities.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AbsenceRepository extends JpaRepository<Absence, Long> {
     List<Absence> findAllByStudentAndSubjectOrderByDate(Student student, Subject subject);
+    Optional<Absence> findAbsenceByStudentAndAndSubject(Student student, Subject subject);
 }
